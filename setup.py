be828ea8dbbf1a95a985e670d82c5ee01dd87753
stonelin <jason840708@gmail.com>
2017-12-26 02:25:14 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-binance',
+    version='0.1.0',
+    packages=['binance'],
+    description='stoneline copy of python-binance',
+    url='https://github.com/stonelin/python-binance',
+    author='stonelin',
+    license='MIT',
+    author_email='',
+    install_requires=['requests'],
+    )
